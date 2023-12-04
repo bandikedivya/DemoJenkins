fecdcc65e329215c4c73758ef72243bcdba58348
bandikedivya <divyacrkdv@gmail.com>
2023-12-04 15:35:26 +0530

    Add project files.

---
+++ DemoJenkins/xUnit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace xUnit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
